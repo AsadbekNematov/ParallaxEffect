7158c8eef483c686bbbaf03679c7aabec70e84ff
Asadbek <asadbekn443@gmail.com>
2023-05-08 02:23:21 -0400

    Initial Commit

---
+++ ParallaxEffect/ParallaxEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxEffectApp.swift
+//  ParallaxEffect
+//
+//  Created by Asadbek Nematov on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
